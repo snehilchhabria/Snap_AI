9e58caf92083f9e4ca651ce4398f61d8ad7c4bee
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-07-01 18:11:03 +0530

    Routing setup

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import snapAIRoutes from './routes/snapAIRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({limit : '50mb'}));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/snapAI', snapAIRoutes);
 
 app.get('/',  async(req,res) => {
     res.send('Hello from SnapAI');
