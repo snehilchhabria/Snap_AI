f9d7ed2e982547a6e42aa59118790873b19b3080
Snehil Chhabria <snehilchhabria143@gmail.com>
2023-07-01 14:19:26 +0530

    server initialization

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({limit : '50mb'}));
+
+app.get('/',  async(req,res) => {
+    res.send('Hello from SnapAI');
+})
+
+const startServer = async () => {
+    app.listen(8080, () => console.log('Server has started on the port'))
+}
+
+startServer();
